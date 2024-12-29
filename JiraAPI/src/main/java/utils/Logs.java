5e82feab2d2ea1a675cbab072c7c643e20b7d00a
AkshayBouwal <akshaybouwal@hotmail.com>
2024-12-29 22:13:17 +0530

    29/12/2024

--- JiraAPI/src/main/java/utils/Logs.java
+++ JiraAPI/src/main/java/utils/Logs.java
@@ -10,7 +10,7 @@ public class Logs {
 
         PrintStream log = null;
         try {
-            log = new PrintStream(System.getProperty("user.dir") + "\\src\\main\\java\\logs\\APILogs-" + LocalDateTime.now().toString().replace("-", "").replace(":", "").replace(".", "") + ".txt");
+            log = new PrintStream(System.getProperty("user.dir") + "/src/main/java/logs/APILogs-" + LocalDateTime.now().toString().replace("-", "").replace(":", "").replace(".", "") + ".txt");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
